e86915234277cd2dd30d9b4219d7728ace41c22a
CozmaCristian <cristiancozma97@gmail.com>
2018-11-10 17:50:38 +0000

    Faza 1

--- README.md
+++
@@ -1,3 +0,0 @@
-# rocketLib
-
-Proiect tehnologii web
---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+const express = require('express')
+
+let app = express()
+
+app.use(express.static('public'))
+
+app.get('/ping', (req, res) =>
+{
+    res.status(200).send('pong')
+})
+
+
+app.listen(8080)
